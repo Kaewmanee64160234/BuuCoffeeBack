a0139afee0784fa0c7d136dd26e91b351ded535c
Kaewmanee <64160234@go.buu.ac.th>
2024-05-09 22:21:58 +0700

    🧚‍♀️ Update imports and add TypeORM module for product types and products in product types module

--- src/product-type-toppings/product-type-toppings.module.ts
+++ src/product-type-toppings/product-type-toppings.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductTypeToppingsService } from './product-type-toppings.service';
 import { ProductTypeToppingsController } from './product-type-toppings.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductTypeTopping } from './entities/product-type-topping.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductTypeTopping])],
   controllers: [ProductTypeToppingsController],
-  providers: [ProductTypeToppingsService]
+  providers: [ProductTypeToppingsService],
 })
 export class ProductTypeToppingsModule {}
--- src/product-type-toppings/product-type-toppings.service.ts
+++ src/product-type-toppings/product-type-toppings.service.ts
@@ -1,26 +1,134 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProductTypeToppingDto } from './dto/create-product-type-topping.dto';
 import { UpdateProductTypeToppingDto } from './dto/update-product-type-topping.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductTypeTopping } from './entities/product-type-topping.entity';
+import { ProductType } from 'src/product-types/entities/product-type.entity';
+import { Topping } from 'src/toppings/entities/topping.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductTypeToppingsService {
-  create(createProductTypeToppingDto: CreateProductTypeToppingDto) {
-    return 'This action adds a new productTypeTopping';
+  constructor(
+    @InjectRepository(ProductType)
+    private productTypeRepository: Repository<ProductType>,
+    @InjectRepository(Topping)
+    private toppingRepository: Repository<Topping>,
+    @InjectRepository(ProductTypeTopping)
+    private productTypeToppingRepository: Repository<ProductTypeTopping>,
+  ) {}
+  async create(createProductTypeToppingDto: CreateProductTypeToppingDto) {
+    try {
+      const productType = await this.productTypeRepository.findOne({
+        where: { productTypeId: createProductTypeToppingDto.productTypeId },
+      });
+      const topping = await this.toppingRepository.findOne({
+        where: { toppingId: createProductTypeToppingDto.toppingId },
+      });
+
+      if (!productType || !topping) {
+        throw new HttpException(
+          'Product, product type or topping not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const newProductTypeTopping = new ProductTypeTopping();
+
+      newProductTypeTopping.productType = productType;
+      newProductTypeTopping.topping = topping;
+      newProductTypeTopping.quantity = createProductTypeToppingDto.quantity;
+
+      return this.productTypeToppingRepository.save(newProductTypeTopping);
+    } catch (error) {
+      throw new HttpException(
+        'Failed to create product type topping',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   findAll() {
-    return `This action returns all productTypeToppings`;
+    try {
+      return this.productTypeToppingRepository.find();
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch product type toppings',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productTypeTopping`;
+  async findOne(id: number) {
+    try {
+      //find if not found throw error
+      const productTypeTopping =
+        await this.productTypeToppingRepository.findOne({
+          where: { productTypeToppingId: id },
+        });
+      if (!productTypeTopping) {
+        throw new HttpException(
+          'Product type topping not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return productTypeTopping;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to fetch product type topping',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  update(id: number, updateProductTypeToppingDto: UpdateProductTypeToppingDto) {
-    return `This action updates a #${id} productTypeTopping`;
+  async update(
+    id: number,
+    updateProductTypeToppingDto: UpdateProductTypeToppingDto,
+  ) {
+    try {
+      const productTypeTopping =
+        await this.productTypeToppingRepository.findOne({
+          where: { productTypeToppingId: id },
+        });
+      if (!productTypeTopping) {
+        throw new HttpException(
+          'Product type topping not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const updatedProductTypeTopping =
+        await this.productTypeToppingRepository.save({
+          ...productTypeTopping,
+          ...updateProductTypeToppingDto,
+        });
+      return updatedProductTypeTopping;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to update product type topping',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} productTypeTopping`;
+  async remove(id: number) {
+    try {
+      const productTypeTopping =
+        await this.productTypeToppingRepository.findOne({
+          where: { productTypeToppingId: id },
+        });
+      if (!productTypeTopping) {
+        throw new HttpException(
+          'Product type topping not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return this.productTypeToppingRepository.delete({
+        productTypeToppingId: id,
+      });
+    } catch (error) {
+      throw new HttpException(
+        'Failed to delete product type topping',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
