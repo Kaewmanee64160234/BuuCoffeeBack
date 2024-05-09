e553115f2ce286b92f95edc09802cce80e9eac2b
Kaewmanee <64160234@go.buu.ac.th>
2024-05-09 23:04:33 +0700

    Update topping validation in CreateToppingDto and create toppingService

--- src/toppings/toppings.controller.ts
+++ src/toppings/toppings.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ToppingsService } from './toppings.service';
 import { CreateToppingDto } from './dto/create-topping.dto';
 import { UpdateToppingDto } from './dto/update-topping.dto';
