0af2997300aa5a55ad4c82259b6041f480d32c6f
Kaewmanee <64160234@go.buu.ac.th>
2024-05-09 22:10:01 +0700

    🧚‍♀️ Update imports and add TypeORM module for product types and products in product types module

--- src/product-types/product-types.module.ts
+++ src/product-types/product-types.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductTypesService } from './product-types.service';
 import { ProductTypesController } from './product-types.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductType } from './entities/product-type.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductType, Product])],
   controllers: [ProductTypesController],
-  providers: [ProductTypesService]
+  providers: [ProductTypesService],
 })
 export class ProductTypesModule {}
